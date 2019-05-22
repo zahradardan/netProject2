e279c4a33d1198ae36ef3761489f40f20a310f9f
sogolalipour <49830872+sogolalipour@users.noreply.github.com>
2019-05-22 23:59:57 +0430

    Add files via upload

---
+++ src/main/java/ir/asta/training/contacts/entities/CaseEntity.java
@@ -0,0 +1,82 @@
+package ir.asta.training.contacts.entities;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name="cases")
+public class CaseEntity {
+
+    Long caseID;
+    String submitter;
+    String topic;
+    String description;
+    String status;
+    String refer;
+    String date;
+
+
+    @Id
+    @Column(name = "CaseID")
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    public Long getId() {
+        return caseID;
+    }
+    public void setId(Long id) {
+        this.caseID = id;
+    }
+
+    @Basic
+    @Column(name="submitter")
+    public String getSubmitter() {
+        return submitter;
+    }
+    public void setSubmitter(String submitter ) {
+        this.submitter =submitter ;
+    }
+
+    @Basic
+    @Column(name="topic")
+    public String getTopic() {
+        return topic;
+    }
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    @Basic
+    @Column(name="description")
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description =description ;
+    }
+
+    @Basic
+    @Column(name="status")
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status ) {
+        this.status =status ;
+    }
+
+    @Basic
+    @Column(name="refer")
+    public String getRefer() {
+        return refer;
+    }
+    public void setRefer(String refer) {
+        this.refer =refer;
+    }
+
+    @Basic
+    @Column(name="date")
+    public String getDate() {
+        return date;
+    }
+    public void setDate(String date ) {
+        this.date = date;
+    }
+}
