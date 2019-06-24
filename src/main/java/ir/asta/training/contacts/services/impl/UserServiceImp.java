c5ac2290f04107978f39db3e9e1c630d31a49723
zahradardan <47085575+zahradardan@users.noreply.github.com>
2019-06-24 13:48:13 -0700

    Add files via upload

--- src/main/java/ir/asta/training/contacts/services/impl/UserServiceImp.java
+++ src/main/java/ir/asta/training/contacts/services/impl/UserServiceImp.java
@@ -13,6 +13,7 @@ import ir.asta.wise.core.datamanagement.ActionResult;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.security.NoSuchAlgorithmException;
+import java.util.List;
 
 @Named("UserService")
 public class UserServiceImp implements UserService {
@@ -21,17 +22,14 @@ public class UserServiceImp implements UserService {
     UserManager manager;
 
     @Override
-
-    public ActionResult<UserEntity> save(String username, String password, boolean isStudent) {
+    public ActionResult<UserEntity> save(String username, String password, boolean isStudent,boolean isTeacher) {
 
         UserEntity entity = new UserEntity();
         entity.setName(username);
         entity.setPassword(password);
-        entity.setStatus(isStudent);
+        entity.setStudent(isStudent);
+        entity.setTeacher(isTeacher);
         return manager.save(entity);
-
-//			return entity.getId() + " " +entity.getName()+" ";
-//			return new ActionResult<Long>(true, "New contact saved successfully.", entity.getId());
     }
 
     @Override
@@ -39,4 +37,23 @@ public class UserServiceImp implements UserService {
         return manager.login(username, password);
     }
 
+    @Override
+    public ActionResult<List<UserEntity>> management(){
+        return manager.management();
+    }
+
+    @Override
+    public  ActionResult<Integer> manager_del(String username){
+        return manager.manage_del(username);
+    }
+
+    @Override
+    public ActionResult<UserEntity> manager_up(String username){
+      return manager.manage_up(username);
+    }
+
+    @Override
+    public ActionResult<UserEntity> manager_act(String username){
+        return manager.manage_act(username);
+    }
 }
