2d19a5d1f402d09f6f8dd34c35cae1510789b07f
sogolalipour <49830872+sogolalipour@users.noreply.github.com>
2019-06-25 01:57:50 +0430

    Add files via upload

--- src/main/java/ir/asta/training/contacts/services/UserService.java
+++ src/main/java/ir/asta/training/contacts/services/UserService.java
@@ -6,12 +6,10 @@ import javax.ws.rs.core.MediaType;
 
 import ir.asta.training.contacts.entities.UserEntity;
 import ir.asta.wise.core.datamanagement.ActionResult;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.security.NoSuchAlgorithmException;
-import java.util.List;
 
 @Path("/contact")
 
@@ -23,7 +21,7 @@ public interface UserService {
 	@Produces(MediaType.APPLICATION_JSON)
 
 	public ActionResult<UserEntity> save(@FormParam("username") String username, @FormParam("password") String password,
-			@FormParam("isStudent") boolean isStudent,@FormParam("isTeacher")boolean isTeacher);
+			@FormParam("isStudent") boolean isStudent);
 
 	@POST
 
@@ -31,28 +29,15 @@ public interface UserService {
 	@Path("/logIn")
 	public ActionResult<UserEntity> load(@FormParam("password") String password,@FormParam("username") String username) throws IOException, NoSuchAlgorithmException, ServletException;
 
-
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	@Path("/management")
-	public ActionResult<List<UserEntity>> management();
-
-
 	@POST
+
 	@Produces(MediaType.APPLICATION_JSON)
-	@Path("/del/{token}")
+	@Path("/edit")
+	public ActionResult<UserEntity> edit(
+			                             @FormParam("oldpassword") String oldpassword,
+										 @FormParam("newpassword") String newpassword,
+										 @FormParam("username") String username
+										)throws IOException, NoSuchAlgorithmException, ServletException;
 
-	public ActionResult<Integer> manager_del(@PathParam("token") String username);
 
-	@POST
-	@Produces(MediaType.APPLICATION_JSON)
-	@Path("/conf/{token}")
-
-	public ActionResult<UserEntity> manager_up(@PathParam("token") String username);
-
-	@POST
-	@Produces(MediaType.APPLICATION_JSON)
-	@Path("/activate/{token}")
-
-	public ActionResult<UserEntity> manager_act(@PathParam("token") String username);
 }
