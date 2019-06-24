b5c765fd256f77bfe42be223edaa9af08722768c
zahradardan <47085575+zahradardan@users.noreply.github.com>
2019-06-24 13:40:21 -0700

    Add files via upload

--- src/main/java/ir/asta/training/contacts/dao/UserDao.java
+++ src/main/java/ir/asta/training/contacts/dao/UserDao.java
@@ -1,5 +1,7 @@
 package ir.asta.training.contacts.dao;
 
+
+
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -7,6 +9,7 @@ import javax.persistence.Query;
 
 import ir.asta.training.contacts.entities.UserEntity;
 import ir.asta.wise.core.datamanagement.ActionResult;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -22,7 +25,7 @@ public class UserDao {
 	}
 
 	public void load(String username ) {
-		Query query = entityManager.createQuery("select e from UserEntity e where e.name = :username ");
+		Query query = entityManager.createQuery("select e from UserEntity e where e.name = :username");
 	}
 
 	public boolean containsUser(String username) {
@@ -41,5 +44,44 @@ public class UserDao {
 		    return null;
 		else
 		    return (UserEntity) result.get(0);
+	}
+
+	public List<UserEntity> management(){
+		Query query = entityManager.createQuery("select e from UserEntity e where e.student = false");
+		List result = query.getResultList();
+
+		if(result.isEmpty()){
+			return null;
+		}else{
+			return result;
+		}
+	}
+
+
+	@Transactional
+	public int manager_del(String username){
+		Query query = entityManager.createQuery("DELETE from UserEntity e where e.name = :username");
+		int p =query.setParameter("username",username).executeUpdate();
+		return p;
+	}
+
+	@Transactional
+	public UserEntity manager_up(String username){
+		Query query = entityManager.createQuery("select e from UserEntity e where e.name = :username");
+		List results = query.setParameter("username", username).getResultList();
+		UserEntity ue = (UserEntity) results.get(0);
+		ue.setConfirmed(true);
+		entityManager.merge(ue);
+		return ue;
+	}
+
+	@Transactional
+	public UserEntity manager_act(String username){
+		Query query = entityManager.createQuery("select e from UserEntity e where e.name = :username");
+		List results = query.setParameter("username", username).getResultList();
+		UserEntity ue = (UserEntity) results.get(0);
+		ue.setActivate(false);
+		entityManager.merge(ue);
+		return ue;
 	}
 }
